fcdf4d6aa9475eb30c7dcd6dbf36455669ad17a1
walkeriano <awalkerbarreda@gmail.com>
2026-07-10 01:58:59 +0100

    chanbes integrated

--- src/components/usersActions/usersActions.jsx
+++ src/components/usersActions/usersActions.jsx
@@ -2,10 +2,7 @@ import styles from "./usersActions.module.css";
 import Image from "next/image";
 import { useUsers } from "@/hooks/useUsers";
 
-export default function UsersActions({
-  selectedUsers,
-  onToggleUser,
-}) {
+export default function UsersActions({ selectedUsers, onToggleUser }) {
   const { users, loading, error } = useUsers();
 
   if (loading) return <p>Cargando usuarios...</p>;
@@ -21,60 +18,29 @@ export default function UsersActions({
         <Image src="/arrow-bottom.svg" width={10} height={12} alt="icon-menu" />
       </section>
       <section className={styles.listUsers}>
-                {users.map((user) => {
-
-          const checked = selectedUsers.some(
-
-            (item) => item.id === user.id
-
-          );
-
+        {users.map((user) => {
+          const checked = selectedUsers.some((item) => item.id === user.id);
           return (
-
-            <section
-
-              key={user.id}
-
-              className={styles.itemUser}
-
-            >
-
+            <section key={user.id} className={styles.itemUser}>
               <div className={styles.imgPerfil}>
-
                 <Image
-
                   src={user.imagenPerfil || "/image-angels.png"}
-
                   fill
-
                   alt="Mentoria Image"
-
                 />
-
               </div>
 
               <div className={styles.infoUser}>
-
                 <h3>{user.nombreUsuario}</h3>
-
                 <p>{user.email}</p>
-
               </div>
-
               <input
-
                 type="checkbox"
-
                 checked={checked}
-
                 onChange={() => onToggleUser(user)}
-
               />
-
             </section>
-
           );
-
         })}
       </section>
     </section>
