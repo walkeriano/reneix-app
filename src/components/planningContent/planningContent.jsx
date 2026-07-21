a29fbc6f49194dd4aa8b17d65e7b274e3c183066
walkeriano <awalkerbarreda@gmail.com>
2026-07-21 15:18:14 +0100

    cambios integrados

--- src/components/planningContent/planningContent.jsx
+++ src/components/planningContent/planningContent.jsx
@@ -1,7 +1,14 @@
 import styles from "./planningContent.module.css";
 import Image from "next/image";
+import Link from "next/link";
+import getNextSession from "@/utils/getNextSession";
 
 export default function PlanningContent({ setView, planning }) {
+  const nextSession = getNextSession(
+    planning?.selectedDays,
+    planning?.sessionHour,
+  );
+
   return (
     <section className={styles.lockerContent}>
       <section className={styles.titleSection}>
@@ -15,7 +22,26 @@ export default function PlanningContent({ setView, planning }) {
         </div>
       </section>
       <section className={styles.calendarSection}>
-        
+        {nextSession && (
+          <section className={styles.nextCita}>
+            <h2>{planning?.title}</h2>
+            <p>
+              {nextSession.date} a las {nextSession.hour} {nextSession.period}
+            </p>
+            <Link href={planning?.linkVideollamada}>Ir a la videollamada</Link>
+          </section>
+        )}
+        <section className={styles.containerListDates}>
+          <div>Lista de sesiones</div>
+          <section className={styles.flexContainer}>
+            {planning?.selectedDays?.map((day) => (
+              <div key={day}>
+                <p>{day}</p>
+                <Image src="/calendar.svg" width={25} height={23} alt="Video Icon" />
+              </div>
+            ))}
+          </section>
+        </section>
       </section>
     </section>
   );
