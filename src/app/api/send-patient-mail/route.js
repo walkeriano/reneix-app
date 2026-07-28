f95712f2203a303d35dad508483ddf18d4199cc8
walkeriano <awalkerbarreda@gmail.com>
2026-07-28 14:50:36 +0100

    cambios estrutura mail

--- src/app/api/send-patient-mail/route.js
+++ src/app/api/send-patient-mail/route.js
@@ -7,25 +7,35 @@ export async function POST(request) {
   try {
     const body = await request.json();
 
-    const { nombreUsuario, email, codigoAcceso, linkVideollamada } = body;
+    const { nombreUsuario, email, codigoAcceso, linkVideollamada, movil, direccion, pais } = body;
 
     await resend.emails.send({
       from: process.env.MAIL_FROM,
       to: email,
-      subject: "Reneix App - Mentoría de Rehaz tu vida",
+      subject: "Reneix App - Mentoría Rehaz tu vida",
 
       html: `
-      <h2>Hola ${nombreUsuario}</h2>
-      <p>Tu psicóloga ha creado tu cuenta.</p>
+      <h2>Hola ${nombreUsuario}, bienvenida a la mentoría Rehaz tu vida</h2>
+      <p>En Reneix App encontrarás todos los detalles importantes sobre tu mentoría, el planeamiento de sesiones, acceso a las videollamadas, interacción directa con Àngels Córcoles, comunidad de pacientes y mucho más!</p>
       <hr>
+      <p>Paciente registrado</p>
+      <p><b>Nombre:</b> ${nombreUsuario}</p>
+      <p><b>Móvil:</b> ${movil}</p>
+      <p><b>Email:</b> ${email}</p>
+      <p><b>Dirección:</b> ${direccion}</p>
+      <p><b>País:</b> ${pais}</p>
+      <hr>
+      <p>Credenciales de acceso</p>
       <p><b>Correo de acceso:</b> ${email}</p>
       <p><b>Código de acceso:</b> ${codigoAcceso}</p>
-      <p><b>Videollamada:</b></p>
-      <a href="${linkVideollamada}">
-        ${linkVideollamada}
+      <hr>
+      <p><b>Acceso Reneix App:</b></p>
+      <a href="/angelscorcoles.com/sesion-usuario">
+        angelscorcoles.com
       </a>
       <br><br>
       <p>Puedes iniciar sesión cuando desees.</p>
+      <p>RENEIX APP / ÀNGELS CÓRCOLES - COPYRIGHT 2026@</p>
       `,
     });
 
